de1b47caadac41cf244cdc6a2172ecdd74556826
ackkie <satsukimasu@gmail.com>
2014-01-21 23:40:46 +0900

    既存より
    webより

--- site-cookbooks/vskel_centos6.5/recipes/default.rb
+++ site-cookbooks/vskel_centos6.5/recipes/default.rb
@@ -7,7 +7,21 @@
 # All rights reserved - Do Not Redistribute
 #
 
+#kickstart後のpostinstall調整分
 %w{xterm git lftp nmap system-config-lvm}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+#yum系
+%w{yum-utils yum-plugin-downloadonly yum-plugin-versionlock yum-plugin-security}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+%w{dstat sysstat}.each do |pkg|
   package pkg do
     action :install
   end
