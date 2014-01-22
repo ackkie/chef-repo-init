daedd7ed8af2d901efd43d5d34598ab0e1b5eb87
ackkie <satsukimasu@gmail.com>
2014-01-22 13:59:51 +0900

    パッケージ追加

--- site-cookbooks/vskel_centos6.5/recipes/default.rb
+++ site-cookbooks/vskel_centos6.5/recipes/default.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #
 # Cookbook Name:: vskel_centos6.5
 # Recipe:: default
@@ -7,21 +8,95 @@
 # All rights reserved - Do Not Redistribute
 #
 
-#kickstart後のpostinstall調整分
-%w{xterm git lftp nmap system-config-lvm}.each do |pkg|
+#
+#yum系
+#
+
+%w{
+yum-plugin-downloadonly
+yum-plugin-security
+yum-plugin-versionlock
+yum-utils
+}.each do |pkg|
   package pkg do
     action :install
   end
 end
 
-#yum系
-%w{yum-utils yum-plugin-downloadonly yum-plugin-versionlock yum-plugin-security}.each do |pkg|
+#
+# SELinux関連
+#
+# policycoreutils-gui system-config-selinuxが入っている
+# policycoreutils-python semanageが入っている
+# setools-console sesearchが入っている
+# setroubleshoot avclogをsyslogやメール出力
+# setroubleshoot-server avclogをsyslogやメール出力
+# star SELinuxアーカイブ
+
+%w{
+policycoreutils-gui
+policycoreutils-python
+setools-console
+setroubleshoot
+setroubleshoot-server
+star
+}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+%w{redhat-lsb}.each do |pkg|
   package pkg do
     action :install
   end
 end
 
 %w{dstat sysstat}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+%w{
+expect
+git
+lftp
+nmap
+screen
+subversion
+tree
+xterm
+}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+#
+# その他
+#
+# freenx nxserver
+# net-snmp
+# net-snmp-utils
+# net-snmp-perl traptoemailに必要
+# webalizer apacheログを見る
+# nfs-utils NFSクライアントで必要
+# nfs4-acl-tools NFSクライアントで必要
+
+%w{
+emacs
+firefox
+freenx
+ipmitool
+net-snmp
+net-snmp-perl
+net-snmp-utils
+nfs-utils
+nfs4-acl-tools
+system-config-lvm
+webalizer
+}.each do |pkg|
   package pkg do
     action :install
   end
