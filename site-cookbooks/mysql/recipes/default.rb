d8f6e631c99ccfafe8d8a089494928c1e45360dd
ackkie <satsukimasu@gmail.com>
2014-01-27 16:52:25 +0900

    不具合修正

--- site-cookbooks/mysql/recipes/default.rb
+++ site-cookbooks/mysql/recipes/default.rb
@@ -1,4 +1,4 @@
-# -*- coding: undecided -*-
+# -*- coding: utf-8 -*-
 #
 # Cookbook Name:: mysql
 # Recipe:: default
@@ -7,27 +7,38 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+#もっとよいかきかたがあるはず
 execute "mysql-server-install" do
   command "yum -y install mysql-server --enablerepo=remi"
   action  :run
-  not_if  { File.exists?('/var/lib/mysql') }
+  not_if  { File.exists?('/etc/rc.d/init.d/mysqld') }
 end
 
-#もっとよいかきかたがあるはず
-execute "service start" do
-  command "service start"
+# ここで/etc/my.cnfを設定
+
+#サービススタート
+execute "mysql-server start" do
+  not_if  { File.exists?('/var/run/mysqld') }
+  command "service mysqld start"
   action  :run
-  not_if  { File.exists?('/var/lib/mysql') }
 end
 
-execute "mysql-install-db" do
-  command "mysql_secure_installation"
-  action  :run
-  not_if  { File.exists?('/var/lib/mysql/mysql/user.frm') }
+script "mysql_secure_installation" do
+  interpreter 'bash'
+  user "root"
+  only_if { File.exists?('/var/lib/mysql/test') }
+  code <<-EOS
+    mysql -u root -e "DELETE FROM mysql.user WHERE User='';"
+    mysql -u root -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+    mysql -u root -e "DROP DATABASE test;"
+    mysql -u root -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';"
+    mysql -u root -e "UPDATE mysql.user SET Password=PASSWORD('hoge') WHERE User='root'"
+    mysql -u root -e "FLUSH PRIVILEGES;"
+  EOS
 end
 
 service "mysqld" do
   supports status: true, restart: true, reload: true
   action   [ :enable, :start ]
 end
-
